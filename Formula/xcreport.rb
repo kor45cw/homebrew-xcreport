26ff5f9f263a5e9c43b2397389d712c23de3bf6e
kor45cw <kor45cw@gmail.com>
2023-04-14 12:09:19 +0900

    Publish 0.0.2

--- Formula/xcreport.rb
+++ Formula/xcreport.rb
@@ -3,9 +3,9 @@
 class Xcreport < Formula
     desc "A Swift command-line tool to manage xcresult file"
     homepage "https://github.com/kor45cw/XCReport"
-    url "https://github.com/kor45cw/XCReport/releases/download/0.0.1/xcreport.tar.gz"
-    sha256 "cb2f1e457041c3fdf3a843cf65e55e69aadf0e8755153fdf6b3a9bfebd676442"
-    version "0.0.1"
+    url "https://github.com/kor45cw/XCReport/releases/download/0.0.2/xcreport.tar.gz"
+    sha256 "b58f8ace4511ca8dc038e5a493027f58be26788a640451d563e13ac97b1a75b4"
+    version "0.0.2"
     
     def install
         bin.install "xcreport"
